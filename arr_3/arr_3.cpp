b8e73713203da8e3ca76edd8e607da3a6c026003
김민수 <kok2209@gmail.com>
2018-11-05 18:46:51 +0900

    add

--- arr_3/arr_3.cpp
+++ arr_3/arr_3.cpp
@@ -7,11 +7,25 @@ int main()
     int n;
     cin >> n;
 
-    int sum=0;
-    
-    sum = n*n;
-
     int num[n][n];
+
+    int count=1;
+    int count2=0;
+    int count3=1;
+
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<n-i; j++ )
+        {    
+             count = count +j+i;
+             num[i][j]=count;
+             cout << num [i][j]<<" ";  
+        } 
+        count2++;
+        count3 = count2 +count3;
+        count = count3;
+        cout << "\n";      
+    }
 
 
     return 0;
